c1a8ef201d5393f6d7bbb082582b88f16bf39863
bagl0025 <bagl0025@umn.edu>
2022-06-06 15:46:01 -0500

    create db connection and schema

--- models/Comment.js
+++ models/Comment.js
@@ -6,43 +6,39 @@ class Comment extends Model {}
 Comment.init(
   {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
       },
-      comment_text: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1]
-          }
-        // references: {
-        //   model: 'user',
-        //   key: 'id'
-        // }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
       },
-      user_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: {
-            model: 'user',
-            key: 'id'
-          }
-        },
-        post_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: {
-            model: 'post',
-            key: 'id'
-          }
-        }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'comment'
+    modelName: 'comment',
   }
 );
 
