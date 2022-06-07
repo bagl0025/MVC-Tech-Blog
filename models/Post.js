efff788f3ab61f1027c69a9b356cad073aeb2aa4
bagl0025 <bagl0025@umn.edu>
2022-06-07 15:09:31 -0500

    finish refactoring, need styling and testing

--- models/Post.js
+++ models/Post.js
@@ -1,30 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {
-  // static upvote(body, models){
-  //     return models.Vote.create({
-  //         user_id: body.user_id,
-  //         post_id: body.post_id
-  //       }).then(() => {
-  //         return Post.findOne({
-  //           where: {
-  //             id: body.post_id
-  //           },
-  //           attributes: [
-  //             'id',
-  //             'post_url',
-  //             'title',
-  //             'created_at',
-  //             [
-  //               sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-  //               'vote_count'
-  //             ]
-  //           ]
-  //         });
-  //       });
-  // }
-}
+class Post extends Model {}
 
 // create fields/columns for Post model
 Post.init(
@@ -39,11 +16,11 @@ Post.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    post_url: {
-      type: DataTypes.STRING,
+    post_content: {
+      type: DataTypes.TEXT,
       allowNull: false,
       validate: {
-        isURL: true,
+        len: [1],
       },
     },
     user_id: {
--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,17 +2,18 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const post_url = document.querySelector('input[name="post-url"]').value;
-
+  const post_content = document.querySelector(
+    'input[name="post_content"]'
+  ).value;
   const response = await fetch(`/api/posts`, {
     method: 'POST',
     body: JSON.stringify({
       title,
-      post_url
+      post_content,
     }),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   });
 
   if (response.ok) {
@@ -22,4 +23,6 @@ async function newFormHandler(event) {
   }
 }
 
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler);
