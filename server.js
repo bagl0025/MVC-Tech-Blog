19b9277301fa5883273b151b06bc32489597506b
bagl0025 <bagl0025@umn.edu>
2022-06-07 13:50:34 -0500

    refactor code

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Supersecret',
-  cookie: { maxAge: 120000 },
+  cookie: { maxAge: 5120000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
